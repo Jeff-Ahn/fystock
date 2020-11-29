bf955b986cccd8de288cf631463e8d33a3a4a710
Chungseob Lee <cndtjq010@naver.com>
2020-11-30 00:07:12 +0900

    feat : set server port 8080

--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
+const http = require('http'); // 서버를 만드는 모듈 불러옴
+const route = require('./route/route.js');
+const compression = require('compression');
+const cors = require('cors');
 const express = require('express');
-
 const app = express();
-
-const PORT = 5000;
-
-app.get('/', (req, res) => {
-  res.send('hello world');
+const db=require('./db.js')
+const bodyParser=require('body-parser');
+db();
+app.use(bodyParser.json());
+app.use('/', route);
+app.use((req, res, next) => { // 404 처리 부분
+  res.status(404).send('일치하는 주소가 없습니다!');
 });
-
-app.listen(PORT, () => {
-  console.log(`App is listening on ${PORT}`);
+app.use((err, req, res, next) => { // 에러 처리 부분
+  console.error(err.stack); // 에러 메시지 표시
+  res.status(500).send('서버 에러!'); // 500 상태 표시 후 에러 메시지 전송
 });
+app.listen(8080);
