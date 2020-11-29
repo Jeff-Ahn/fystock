935cd8ac826d9339fbbab69372bc2a35f7c8954f
Chungseob Lee <cndtjq010@naver.com>
2020-11-30 00:03:09 +0900

    feat : set data schema

---
+++ server/model/Financial.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+let FinancialSchema = new Schema({
+  code : String,
+  date : String,
+  total : Number,
+  sell : Number,
+  pure:Number,
+  sell_per:Number,
+  pure_per:Number,
+  roe:Number,
+  debut_per:Number,
+  quick_ratio:Number,
+  reserv_ratio:Number,
+  eps:Number,
+  eps_per:Number,
+  bps:Number,
+  bps_per:Number,
+  weekly:Number,
+  timely:Number,
+  trend:Number
+});
+
+module.exports = mongoose.model("Financial", FinancialSchema);
